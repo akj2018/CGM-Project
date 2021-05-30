9ac0d13b558753eacedaf8d258a6e3c8d1760422
shubhamm2712 <shubham.189301129@muj.manipal.edu>
2021-05-31 01:50:02 +0530

    final

--- zombie.py
+++ zombie.py
@@ -1,28 +1,4 @@
-############################################################################################
-###                                                                                      ###
-###   PyGame with a SnowPea shoot bullet for defensing the zombie army coming            ###
-###                                                                                      ###
-###   Author: Junjie Shi                                                                 ###
-###   Email : handsomestone@gmail.com                                                    ### 
-###                                                                                      ### 
-###   Do Enjoy the game!                                                                 ###
-###   You need to have Python and PyGame installed to run it.                            ###
-###   Run it by typing "python zombie.py" in the terminal                                ###
-###                                                                                      ### 
-###   This program is free software: you can redistribute it and/or modify               ### 
-###   it under the terms of the GNU General Public License as published by               ### 
-###   the Free Software Foundation, either version 3 of the License, or                  ### 
-###   (at your option) any later version.                                                ### 
-###                                                                                      ### 
-###   This program is distributed in the hope that it will be useful,                    ### 
-###   but WITHOUT ANY WARRANTY; without even the implied warranty of                     ### 
-###   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      ### 
-###   GNU General Public License for more details.                                       ### 
-###                                                                                      ### 
-###   You should have received a copy of the GNU General Public License                  ### 
-###   along with this program.  If not, see <http://www.gnu.org/licenses/>.              ###                                                                              ###
-###                                                                                      ###
-############################################################################################
+###   PyGame with a SnowPea shoot bullet for defensing the zombie army coming  
 
 import pygame, random, sys, time
 from pygame.locals import *
